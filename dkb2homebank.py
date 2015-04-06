c63ef3141996d3e0eb9c675e8121dedcde9270e4
¨Ham <hermann.vocke@gmail.com>
2015-04-06 14:26:42 +0200

    removed class structure, renamed converter script

---
+++ dkb2homebank.py
@@ -0,0 +1,111 @@
+import argparse
+import csv
+from datetime import datetime
+
+class dkb(csv.Dialect):
+    delimiter = ';'
+    quotechar = '"'
+    doublequote = True
+    skipinitialspace = False
+    lineterminator = '\r\n'
+    quoting = csv.QUOTE_MINIMAL
+
+csv.register_dialect("dkb", dkb)
+
+dkbFieldNames = ["buchungstag",
+                  "wertstellung",
+                  "buchungstext",
+                  "beguenstigter",
+                  "verwendungszweck",
+                  "kontonummer",
+                  "blz",
+                  "betrag",
+                  "glaeubigerID",
+                  "mandatsreferenz",
+                  "kundenreferenz"]
+
+
+visaFieldNames =    ["abgerechnet",
+                     "wertstellung",
+                     "belegdatum",
+                     "umsatzbeschreibung",
+                     "betrag",
+                     "urspruenglicherBetrag"]
+
+homebankFieldNames = ["date",
+                      "paymode",
+                      "info",
+                      "payee",
+                      "memo",
+                      "amount",
+                      "category",
+                      "tags"]
+
+def skipHeader(reader):
+    for i in range(1, 6):
+        next(reader)
+
+def convertDkbCash(filename):
+    with open(filename, 'r') as csvfile:
+        reader = csv.DictReader(csvfile, dialect='dkb', fieldnames=dkbFieldNames)
+
+        with open("cashHomebank.csv", 'w') as csvfile:
+            skipHeader(reader)
+
+            writer = csv.DictWriter(csvfile, dialect='dkb', fieldnames=homebankFieldNames)
+            for row in reader:
+                writer.writerow(
+                    {
+                    'date': convertDate(row["buchungstag"]),
+                    'paymode': 8,
+                    'info': None,
+                    'payee': row["beguenstigter"],
+                    'memo': row["verwendungszweck"],
+                    'amount': row["betrag"],
+                    'category': None,
+                    'tags': None
+                    })
+
+def convertVisa(filename):
+    with open(filename, 'r') as csvfile:
+        reader = csv.DictReader(csvfile, dialect='dkb', fieldnames=visaFieldNames)
+
+        with open("visaHomebank.csv", 'w') as csvfile:
+            skipHeader(reader)
+            writer = csv.DictWriter(csvfile, dialect='dkb', fieldnames=homebankFieldNames)
+            for row in reader:
+                writer.writerow(
+                    {
+                    'date': convertDate(row["wertstellung"]),
+                    'paymode': 1,
+                    'info': None,
+                    'payee': None,
+                    'memo': row["umsatzbeschreibung"],
+                    'amount': row["betrag"],
+                    'category': None,
+                    'tags': None
+                    })
+
+
+def convertDate(dateString):
+    date = datetime.strptime(dateString, "%d.%m.%Y")
+    return date.strftime('%d-%m-%Y')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Convert a CSV export file from DKB online banking to a Homebank compatible CSV format.")
+    parser.add_argument("filename", help="The CSV file to convert.")
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-v", "--visa", action="store_true", help="convert a DKB Visa account CSV file")
+    group.add_argument("-c", "--cash", action="store_true", help="convert a DKB Cash account CSV file")
+
+    args = parser.parse_args()
+
+    if args.visa:
+        convertVisa(args.filename)
+        print("DKB Visa file converted. Output file: 'visaHomebank.csv'")
+    elif args.cash:
+        convertDkbCash(args.filename)
+        print("DKB Cash file converted. Output file: 'cashHomebank.csv'")
+    else:
+        print("You must provide the type of the CSV file (--cash for DKB Cash, --visa for DKB Visa)")
