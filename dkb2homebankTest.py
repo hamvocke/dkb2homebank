ce95dbdc78e87ed9fe7ab5d43d0654acd3af0e1f
Alexander Blesius <git@blesius.eu>
2020-07-23 12:36:03 +0200

    Fix unit tests

--- dkb2homebankTest.py
+++ dkb2homebankTest.py
@@ -14,29 +14,36 @@ class DKB2HomebankTest(unittest.TestCase):
         warnings.simplefilter("ignore", ResourceWarning)
 
     def testShouldConvertCashFile(self):
-        dkb2homebank.convert_DKB_cash('testfiles/cash.csv', 'cashHomebank.csv')
-        lineNumber = sum(1 for line in open('cashHomebank.csv'))
-        self.assertEqual(lineNumber, 2)
+        with open(r'testfiles/cash.csv', encoding='iso-8859-1') as csv_file:
+            dkb2homebank.convert_DKB_cash(csv_file, 'cashHomebank.csv')
+            csv_file.seek(0)
+            lineNumber = len(dkb2homebank.find_transaction_lines(csv_file))
+            self.assertEqual(lineNumber, 2)
 
     def testShouldConvertCashFileAndWriteToAlternativeOutputDir(self):
-        tmpdir = tempfile.gettempdir()
-        dkb2homebank.convert_DKB_cash('testfiles/cash.csv', os.path.join(tmpdir, "cashHomebank.csv"))
+        with open(r'testfiles/cash.csv', encoding='iso-8859-1') as csv_file:
+            tmpdir = tempfile.gettempdir()
+            dkb2homebank.convert_DKB_cash(csv_file, os.path.join(tmpdir, "cashHomebank.csv"))
 
     def testThrowErrorForEmptyCashFile(self):
         with self.assertRaises(ValueError) as context:
-            dkb2homebank.convert_DKB_cash('testfiles/cash_empty.csv')
-
+            with open('testfiles/cash_empty.csv', encoding='iso-8859-1') as csv_file:
+                dkb2homebank.convert_DKB_cash(csv_file)
         self.assertTrue("Can't convert CSV file without header line" in str(context.exception))
 
     def testShouldConvertVisaFile(self):
-        dkb2homebank.convert_visa('testfiles/visa.csv', 'visaHomebank.csv')
-        lineNumber = sum(1 for line in open('visaHomebank.csv'))
+        with open('testfiles/visa.csv', encoding='iso-8859-1') as csv_file:
+            dkb2homebank.convert_visa(csv_file, 'visaHomebank.csv')
+            csv_file.seek(0)
+            lineNumber = len(dkb2homebank.find_transaction_lines(csv_file))
         self.assertEqual(lineNumber, 4)
 
     def testShouldConvertVisaFileWithRange(self):
-        dkb2homebank.convert_visa('testfiles/visaRange.csv', 'visaHomebank.csv')
-        lineNumber = sum(1 for line in open('visaHomebank.csv'))
-        self.assertEqual(lineNumber, 1)
+        with open('testfiles/visaRange.csv', encoding='iso-8859-1') as csv_file:
+            dkb2homebank.convert_visa(csv_file, 'visaHomebank.csv')
+            csv_file.seek(0)
+            lineNumber = len(dkb2homebank.find_transaction_lines(csv_file))
+            self.assertEqual(lineNumber, 1)
 
     def tearDown(self):
         self.delete('cashHomebank.csv')
